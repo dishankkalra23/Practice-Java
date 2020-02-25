e703af166e05d4f7a80a7aa9173fdd0e97a0a282
Dishank Kalra <dishankalra@gmail.com>
2020-02-25 20:13:33 +0530

    Classes and Objects

---
+++ ClassObj.java
@@ -0,0 +1,15 @@
+// Define class
+class ClassObj{
+    // Initialise fields/instance variable
+    int var = 2;
+
+    // Creating main method/method
+
+    public static void main(String[] args){
+        // Creating object of the class
+        // 'obj' is the object of class 'Classobj'
+        ClassObj obj = new ClassObj();
+        System.out.println("Value of variable: " + obj.var);
+    }
+
+}
